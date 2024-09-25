642617615581d187a09ca10c50a08b158f0812ce
Maximiliano Sandoval <msandova@protonmail.com>
2024-09-25 19:17:29 +0200

    Run ruff format

--- src/flatpak_curl.py
+++ src/flatpak_curl.py
@@ -1,4 +1,4 @@
-__license__ = 'MIT'
+__license__ = "MIT"
 
 import argparse
 import json
@@ -16,15 +16,15 @@ from ruamel.yaml.representer import RoundTripRepresenter
 
 def download(url: str, tempdir: str) -> str:
     with urllib.request.urlopen(url) as response:
-        file_path = os.path.join(tempdir, url.split('/')[-1])
-        with open(file_path, 'x+b') as tar_file:
+        file_path = os.path.join(tempdir, url.split("/")[-1])
+        with open(file_path, "x+b") as tar_file:
             shutil.copyfileobj(response, tar_file)
     return file_path
 
 
 def get_file_hash(filename: str) -> str:
     sha = hashlib.sha256()
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         while True:
             data = f.read(1024 * 1024 * 32)
             if not data:
@@ -35,38 +35,36 @@ def get_file_hash(filename: str) -> str:
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('url')
+    parser.add_argument("url")
     parser.add_argument(
-        '-y',
-        '--yaml',
-        action='store_true',
-        help='generate in yaml format',
+        "-y",
+        "--yaml",
+        action="store_true",
+        help="generate in yaml format",
     )
     opts = parser.parse_args()
     url = opts.url
 
-    filename = url.split('/')[-1]
+    filename = url.split("/")[-1]
 
     name, extension = os.path.splitext(filename)
-    name = name.replace('.tar', '')
+    name = name.replace(".tar", "")
 
-    prefix = 'flatpak-curl-generator'
+    prefix = "flatpak-curl-generator"
     with tempfile.TemporaryDirectory(prefix=prefix) as tempdir:
         filename = download(url, tempdir)
         sha256 = get_file_hash(filename)
-        if extension in ['.gz', '.zip', '.xz', '.bz2']:
-            file_type = 'archive'
+        if extension in [".gz", ".zip", ".xz", ".bz2"]:
+            file_type = "archive"
         else:
-            file_type = 'file'
-    source = OrderedDict(
-        [('type', file_type), ('url', url), ('sha256', sha256)]
-    )
+            file_type = "file"
+    source = OrderedDict([("type", file_type), ("url", url), ("sha256", sha256)])
     module = OrderedDict(
         [
-            ('name', name),
-            ('buildsystem', 'simple'),
-            ('build-commands', []),
-            ('sources', [source]),
+            ("name", name),
+            ("buildsystem", "simple"),
+            ("build-commands", []),
+            ("sources", [source]),
         ]
     )
     if opts.yaml:
@@ -79,8 +77,9 @@ def print_yaml(module: OrderedDict) -> None:
     class MyRepresenter(RoundTripRepresenter):
         pass
 
-    ruamel.yaml.add_representer(OrderedDict, MyRepresenter.represent_dict,
-                                representer=MyRepresenter)
+    ruamel.yaml.add_representer(
+        OrderedDict, MyRepresenter.represent_dict, representer=MyRepresenter
+    )
 
     yaml = ruamel.yaml.YAML()
     yaml.Representer = MyRepresenter
@@ -88,5 +87,5 @@ def print_yaml(module: OrderedDict) -> None:
     yaml.dump(module, sys.stdout)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
